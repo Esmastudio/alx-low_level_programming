5e0aa3f4d1749cf1af3c7b68ec510c96e936585d
esmastudio <esmastudio.co@gmail.com>
2023-04-26 08:41:36 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - prints the addition of two integers
+ * @a: first input
+ * @b: second input
+ * Return: value of addition
+ */
+int add(int a, int b)
+{
+	int addi = 0;
+
+	addi = a + b;
+	return (addi);
+}
